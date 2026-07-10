6d45e317057cc7dfc2035324977a6ca133eecade
Raju Naidu <67865004+arnob32@users.noreply.github.com>
2026-07-10 18:39:24 +0200

    Delete cs.py

--- cs.py
+++
@@ -1,8 +0,0 @@
-﻿from dotenv import load_dotenv
-load_dotenv()
-import requests
-r = requests.get('http://127.0.0.1:8000/api/student-directory')
-d = r.json()
-print('Papers:', len(d))
-for p in d:
-    print(p.get('submission_id'), p.get('status'), p.get('student_name'))
